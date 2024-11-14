816d2116cd9395dc7cdb5bdc2ad4545c62a0bd0c
wesley <wesleywaka77@gmail.com>
2024-11-14 15:04:32 +0300

    fix:protect login

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,13 @@
     <header id="header" class="bg-gray-700">
         <nav class="container mx-auto flex justify-start items-center py-5 px-4">
             <!-- App Name -->
-            <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/" exact-active-class="no-active">Music</router-link>
+            <router-link class="text-white font-bold uppercase text-2xl mr-4" :to="{name: 'home'}" exact-active-class="no-active">Music</router-link>
 
             <div class="flex flex-grow items-center">
                 <!-- Primary Navigation -->
                 <ul class="flex flex-row mt-1">
                     <li>
-                        <router-link href="px-2 text-white" to="/about">About</router-link>
+                        <router-link href="px-2 text-white" :to="{name: 'about'}">About</router-link>
                     </li>
                     <!-- Navigation Links -->
                     <li v-if="!useStore.userLoggedIn">
@@ -16,7 +16,7 @@
                     </li>
                     <template v-else>
                         <li>
-                            <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+                            <router-link class="px-2 text-white" :to="{name:'manage'}">Manage</router-link>
                         </li>
                         <li>
                             <a class="px-2 text-white" href="#" @click.prevent="userStore.signOut">Logout</a>
@@ -36,14 +36,21 @@ import useUserStore from "@/stores/user";
 export default {
     name: 'Header',
     computed: {
-        ...mapStores(useModalStore),
+        ...mapStores(useModalStore,useUserStore),
     }
     methods: {
         toggleAuthModal() {
             this.modalStore.isOpen = !this.modalStore.isOpen;
             console.log(this.modalStore.isOpen);
+        },
+        signOut(){
+            this.userStore.signOut();
+
+            // console.log(this.$route);
+            if(this.$route.meta.requiresAuth){
+                this.$route.push({name: "home"});
+            }
         }
     }
 }
-
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,40 @@ import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home.vue';
 import About from "@/views/About.vue";
 import Manage from "@/views/Manage.vue";
+import useUserStore from "@/stores/user";
+
 
 const routes = [
   {
+    name: 'home',
     path: "/",
     component: Home, 
   },
   {
+    name: 'about',
     path: '/about',
     component: About,
   },
   {
-    path: "/manage",
+    name: 'manage',
+    // alias: '/manage',
+    path: "/manage-music",
     component: Manage,
+    beforeEnter: (to,from,next) => {
+      console.log('Mange Route Guard');
+      next();
+    },
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/manage',
+    redirect: {name: 'manage'},
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: {name: 'home'}
   }
 ];
 
@@ -23,5 +44,22 @@ const router = createRouter({
   routes,
   linkActiveClass: 'text-yellow-500'
 })
+
+router.beforeEach((to,from,next)=>{
+  //console.log(to.meta);
+
+  if(!to.meta.requiresAuth){
+    next();
+    return;
+  }
+
+  const store = useUserStore();
+
+  if(store.userLoggedIn){
+    next();
+  } else{
+    next({name: 'home'})
+  }
+});
 
 export default router;
